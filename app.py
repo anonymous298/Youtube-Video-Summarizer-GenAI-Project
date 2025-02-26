6a225a2a7eb8340954b1294dfb226decaf74a460
anonymous298 <tackletalha@gmail.com>
2025-02-26 21:54:12 +0500

    chatgroq initialized

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 # Loading Necessary Dependencies
 import streamlit as st
+import validators
 
 from langchain_community.document_loaders import YoutubeLoader
 from langchain.prompts import PromptTemplate
 from langchain.chains.summarize import load_summarize_chain
-from langchain_ollama import ChatOllama
+# from langchain_ollama import ChatOllama
+from langchain_groq import ChatGroq
 
 # Creating our main logic
 class YoutubeSummarizer:
@@ -20,23 +22,27 @@ class YoutubeSummarizer:
             loader = YoutubeLoader(video_id, add_video_info=False)
             documents = loader.load()
 
-            return documents
+            return documents 
 
         except Exception as e:
             print(e)
+            st.error(e)
 
-    def summarize_video(self, url: str):
+    def summarize_video(self, url: str, api_key: str):
         '''
         This method will take url and summarize it and returns it
         '''
 
         try:
             # Intializing LLM
-            llm = ChatOllama(model = 'gemma:2b')
+            llm = ChatGroq(
+                model = 'Gemma-7b-It',
+                groq_api_key = api_key
+            )
 
             # Creating our prompt template
             template = """
-            Summarize the following video transcript:
+            Summarize the following video transcript in 300 words:
             {text}
             """
 
@@ -48,6 +54,7 @@ class YoutubeSummarizer:
                 llm = llm, 
                 chain_type = 'stuff',
                 prompt = prompt,
+                verbose=True
             )
 
             # Loading our documents
@@ -60,19 +67,29 @@ class YoutubeSummarizer:
         
         except Exception as e:
             print(e)
+            st.error(e)
 
 # Creating our Streamlit app
 st.title('Youtube Video Summarizer')
 st.write('summarize youtube video with llm')
 
+with st.sidebar:
+    groq_api_key = st.text_input('Groq API Key', type='password', placeholder='Enter your Groq API Key')
+
+if not groq_api_key:
+    st.error('Please provide a API key for Groq LLM')
+
 url = st.text_input('Enter Youtube URL...')
+
+if not validators.url(url):
+    st.error('Please provide a valid Youtube URL')
 
 if st.button('Summarize Video'):
     youtube_summarizer = YoutubeSummarizer()
-    response = youtube_summarizer.summarize_video(url=url)
+    response = youtube_summarizer.summarize_video(url=url, api_key=groq_api_key)
 
     if response:
         st.success(response)
 
     else:
-        st.warning('Error Occured While Summarizing Video')
+        st.error('Error Occured While Summarizing Video')
